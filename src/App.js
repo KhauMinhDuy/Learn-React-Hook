268af4e7f96b7f6b64d6ffb42fd8d3ca32f1a0b1
khauminhduy <khauminhduy@gmail.com>
2022-10-19 10:05:02 +0700

    useRef

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
-import React, { useMemo, useState } from "react";
-
-function expensiveFunction(number) {
-  console.log("start");
-  const start = new Date();
-
-  while (new Date() - start < 3000) {}
-
-  console.log("end", new Date() - start, "ms");
-
-  return number * number;
-}
+import React, { useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-	const [num, setNum] = useState(20);
 
-  // const number = expensiveFunction(10);
-  const number = useMemo(() => {
-    return expensiveFunction(num);
-  }, [num]);
+  // const countRef = useRef(0); // singleton
+	const ref = useRef();
+
+  // const obj = {
+  //   current: 0,
+  // }; // => khi component render lai thi obj duoc khoi tao lai
+
+  // const [count, setCount] = useState(0);
+
+  // console.log({
+  //   count,
+  //   countRef,
+  //   obj,
+  // });
+
+	// useEffect(() => {
+	// 	setInterval(() => {
+	// 		countRef.current = countRef.current + 1;
+	// 		// console.log({countRef});	
+	// 	}, 1000);
+	// }, []);
+
+  // const handleClick = () => {
+  //   // setCount(count + 1);
+  //   countRef.current = countRef.current + 1;
+  //   obj.current = obj.current + 1;
+  // };
+
+
+	console.log({ref});
+	useEffect(() => {
+		ref.current.focus();
+	}, []);
 
   return (
     <>
-      <p>Count {count}</p>
-      <button onClick={() => setCount(count + 1)}>Add</button>
-
-      <p>Number: {number}</p>
+			<input type="text" ref={ref}/>
+      <button>Click Button</button>
     </>
   );
 }
